5ab465aed5e5a85698283fd7fa31bb9f1f3e899a
qiudebo <qiudebo@gmail.com>
2017-08-23 14:43:00 +0800

    probability distributions

--- code/stat/stat_random.py
+++ code/stat/stat_random.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 
 # s = np.random.uniform(-1,0,1000)
@@ -14,10 +15,7 @@ import matplotlib.pyplot as plt
 # print ignored
 # plt.plot(bins, np.ones_like(bins), linewidth=2, color='r')
 # plt.show()
-n, p = 20, .2
-s = np.random.binomial(n, p, 10)
+n, p = 10, .3
+k = np.arange(0,10)
+s = stats.binom.pmf(k, n, p)
 print s
-
-cnt = np.sum(s)
-print s/float(200)
-
