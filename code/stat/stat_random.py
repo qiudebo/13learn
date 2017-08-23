499e92efd200af05d079e4bb4a4632e0cebaf3da
qiudebo <qiudebo@gmail.com>
2017-08-23 19:07:54 +0800

    stat

--- code/stat/stat_random.py
+++ code/stat/stat_random.py
@@ -1,21 +1,12 @@
-# uniform distribution
+# -*- coding: utf-8 -*-
 
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.stats as stats
+from numpy import array
+from numpy.random import normal, randint
 
+data1 = np.array([1, 2, 3])  # 使用ndarray来创建一维数组
+data2 = np.random.normal(0, 1, size=100)  # 随机产生一组均值为0、方差为1的正态分布的结果
+data3 = np.random.randint(0, 2, size=100)  # 随机产生一组100次抛硬币的结果
 
-# s = np.random.uniform(-1,0,1000)
-# print len(s)
-# np.all(s>=-1)
-# np.all(s<0)
-# count, bins, ignored = plt.hist(s, 15, normed=True)
-# print count
-# print bins
-# print ignored
-# plt.plot(bins, np.ones_like(bins), linewidth=2, color='r')
-# plt.show()
-n, p = 10, .3
-k = np.arange(0,10)
-s = stats.binom.pmf(k, n, p)
-print s
+print data1
+print data2
+print data3
