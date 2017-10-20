4d5fbe8edecd406dda4dfc900b3e8f718ed71cab
qiudebo <qiudebo@gmail.com>
2017-10-20 10:17:05 +0800

    C

---
+++ code/c++/exam04.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+Struct Date{
+	int month;
+	int day;
+	int year;
+}
+Struct Student{
+	int num;
+	char[20] name;
+	char sex;
+	int age;
+    Date birthday;
+	float score;
+	char[30] address;
+}stu[3] = {{10001,'Tom','M',18,5,26,1990,92.5,'BeiJing'},
+           {10002,'Tim','M',19,1,1,1990,98.5,'BeiJing'},
+           {10003,'Rose','F',18,9,21,1989,99.5,'BeiJing'}};
+
+
+
+
